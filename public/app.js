0bc83b3a904fa928ebf51e5fa3622e52ecfb1e7a
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-26 14:46:06 +0200

    Trying to make the sum for the player work

--- public/app.js
+++ public/app.js
@@ -5,6 +5,9 @@ Cards = [color, value];
 
 var CardsPlayed = [];
 
+var OutputCards = document.getElementById('outputcards');
+
+
 DrawCardFn = () => {
     var DrawRandomCard = (max) => Math.floor(Math.random(Cards) * max);
     color = DrawRandomCard(4);
@@ -12,10 +15,9 @@ DrawCardFn = () => {
     CardDrawn = {color, value};
     CheckCard(`${CardDrawn.color}, ${CardDrawn.value}`);
     console.table(Cards);
-    console.table(CardsPlayed)
 };
 
-CheckCard = (color) => {
+CheckCard = () => {
     if (CardsPlayed.includes(`${CardDrawn.color}, ${CardDrawn.value}`)) {
         DrawCardFn();
     } else {
@@ -24,22 +26,43 @@ CheckCard = (color) => {
     };
 };
 
-StartGame = () => {    
+StartGame = (OutputCards) => {    
+    OutputCards = document.getElementById('outputcards');
+    OutputCards.innerHTML = '';
     Cards = [];
     CardsPlayed = [];
     for (i = 0; i < 2; i++) {
         DrawCardFn();
+        Write();
     };
 
     console.log(Cards)
 };
 
 
-Hit = () => {
+Hit = (OutputCards) => {
     DrawCardFn();
     console.log(Cards);
+    Write();
 
 };
+
+Write = () => {
+    OutputCards = document.getElementById('outputcards');
+    OutputCards.innerHTML += (`${color}, ${value} <br>`);
+
+    var PlayerSum = document.getElementById('outputsum')
+
+    let Total = 0;
+
+    for (var i=0; i < Cards.lenght; i++){
+        sum += Cards[i].value+1;
+    };
+
+    PlayerSum.innerHTML = Total;
+
+
+}
 
 Add = () => {
     
