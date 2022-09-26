bca8df54d4d0523db180574dedf0e377865c9d9e
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-26 12:49:06 +0200

    Added an array which contains played cards, so it can be checked

--- public/app.js
+++ public/app.js
@@ -1,55 +1,43 @@
-var x;
-var y;
+var color;
+var value;
 
-Cards = [x,y];
-
-var CardCharacteristics = {
-    Hearts: '',
-    Diamonds: '',
-    Clubs: '',
-    Spades: ''
-};
+Cards = [color, value];
 
 var CardsPlayed = [];
 
 DrawCardFn = () => {
     var DrawRandomCard = (max) => Math.floor(Math.random(Cards) * max);
-    /*
-    En loop som först kollar efter om kortet som "dragits" är false(kanske måste leta efter true för att "korten" ej har värdet false som standard) 
-    och i fall att det är det "dela" ut det, och sedan sätt det kortet till true. Om kortet däremot är true ska det genereras ett nytt kort som "delas" ut.
-    Detta ska köras för varje kort så att inte två av samma kort delas ut vid start av nytt spel.
-    Loopen bör då köras för varje DrawRandomCard funktion.
-    */
-    
-    //var falseCond = new Boolean(false);
-    var CardDrawn = (`${DrawRandomCard(4)}, ${DrawRandomCard(13)}`);
-    CheckCard(CardDrawn);
-    console.table(CardsPlayed);
+    color = DrawRandomCard(4);
+    value = DrawRandomCard(13);
+    CardDrawn = {color, value};
+    CheckCard(`${CardDrawn.color}, ${CardDrawn.value}`);
+    console.table(Cards);
+    console.table(CardsPlayed)
 };
 
-CheckCard = (CardDrawn ) => {
-    if (CardsPlayed.includes(CardDrawn)) {
+CheckCard = (color) => {
+    if (CardsPlayed.includes(`${CardDrawn.color}, ${CardDrawn.value}`)) {
         DrawCardFn();
     } else {
-        CardsPlayed.push(CardDrawn);
+        Cards.push(CardDrawn);
+        CardsPlayed.push(`${CardDrawn.color}, ${CardDrawn.value}`);
     };
-
 };
 
-StartGame = () => {
+StartGame = () => {    
+    Cards = [];
     CardsPlayed = [];
-    
     for (i = 0; i < 2; i++) {
         DrawCardFn();
     };
 
-    console.log(CardsPlayed)
+    console.log(Cards)
 };
 
 
 Hit = () => {
     DrawCardFn();
-    console.log(CardsPlayed);
+    console.log(Cards);
 
 };
 
